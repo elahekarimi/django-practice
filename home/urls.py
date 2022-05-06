8cd2c20a291133614bdd955547f3203c77a1b45f
elahe karimi <elikarimi7019@gmail.com>
2022-05-06 23:54:18 +0430

    initial commit

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.home),
     path('hello/', views.say_hello),
+    path('blog/', views.blog),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
+def blog(request):
+    return render(request, 'blog.html')
+
+def home(request):
+    return render(request, 'home.html')
+
 def say_hello(request):
-    return HttpResponse("hello world ...")
+    return render(request, 'hello.html')
